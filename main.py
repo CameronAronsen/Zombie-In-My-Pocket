23f491d24ba26f178b9fba7d9450854957ce962c
ajp0406 <alecjrp1@gmail.com>
2022-03-07 12:48:18 +1300

    Latest

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from directions import Direction
+
+
 class Game:
     def __init__(self, player, time=9, tiles=None, available_tiles=None):
         if available_tiles is None:
             available_tiles = []  # Will contain a list of all available tiles
         if tiles is None:
-            tiles = {}  # Tiles dictionary will have the x and y coords as the key and the Tile object as the value
+            tiles = {}  # Tiles dictionary will have the x and y co-ords as the key and the Tile object as the value
         self.player = player
         self.time = time
         self.available_tiles = available_tiles
@@ -13,8 +16,20 @@ class Game:
         pass
 
     def get_game(self):
+        print(self.available_tiles)
         return print(f'The player has {self.player.get_health()} health and {self.player.get_attack()} attack the '
                      f'time is {self.time}')
+
+    def load_tile(self, name, tile_type):  # Needs Error handling in this function
+        try:
+            if tile_type == "Indoor":
+                tile = IndoorTile(name)
+                self.available_tiles.append(tile)
+            elif tile_type == "Outdoor":
+                tile = OutdoorTile(name)
+                self.available_tiles.append(tile)
+        except ValueError:
+            print("Value Error")
 
 
 class Player:
@@ -41,13 +56,40 @@ class DevCards:
 
 
 class Tile:
-    def __init__(self, doors):
-        self.doors = doors
+    def __init__(self, name, entrance=None, ex=None):
+        self.name = name
+        self.entrance = entrance
+        self.exit = ex
+
+    def set_exit(self, direction):
+        self.exit = direction
+
+    def set_entrance(self, direction):
+        self.entrance = direction
+
+
+class IndoorTile(Tile):
+    def __init__(self, name, entrance=None, ex=None):
+        self.type = "Indoor"
+        super().__init__(name, entrance, ex)
+
+    def __repr__(self):
+        return f'This tile is {self.name}, {self.entrance}, {self.exit}, {self.type}'
+
+
+class OutdoorTile(Tile):
+    def __init__(self, name, entrance=None, ex=None):
+        self.type = "Outdoor"
+        super().__init__(name, entrance, ex)
+
+    def __repr__(self):
+        return f'This tile is {self.name}, {self.entrance}, {self.exit}, {self.type}'
 
 
 def main():
     player = Player()
     game = Game(player)
+    game.load_tile("Foyer", "Indoor")
     game.get_game()
 
 
