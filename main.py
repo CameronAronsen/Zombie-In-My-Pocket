e29b2ca2dcc5244a2eef02968e33c29d5c860a1f
alecjrp-sudo <alecjrp1@gmail.com>
2022-03-07 19:45:19 +1300

    Game Tiles Working

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from directions import Direction
+from directions import Direction as dir
+import pandas as pd
 
 
 class Game:
@@ -12,24 +13,33 @@ class Game:
         self.available_tiles = available_tiles
         self.tiles = tiles
 
+    def start_game(self):
+        for tile in self.available_tiles:
+            if tile.name == 'Patio':
+                tile.set_exit(dir.NORTH)
+                patio = tile
+                self.tiles[(patio.x, patio.y)] = patio
+                break
+
     def draw_card(self):
         pass
 
     def get_game(self):
-        print(self.available_tiles)
         return print(f'The player has {self.player.get_health()} health and {self.player.get_attack()} attack the '
                      f'time is {self.time}')
 
-    def load_tile(self, name, tile_type):  # Needs Error handling in this function
-        try:
-            if tile_type == "Indoor":
-                tile = IndoorTile(name)
-                self.available_tiles.append(tile)
-            elif tile_type == "Outdoor":
-                tile = OutdoorTile(name)
-                self.available_tiles.append(tile)
-        except ValueError:
-            print("Value Error")
+    def load_tiles(self):  # Needs Error handling in this function
+        excel_data = pd.read_excel('Tiles.xlsx')
+        tiles = []
+        for name in excel_data.iterrows():
+            tiles.append(name[1].tolist())
+        for tile in tiles:
+            if tile[2] == "Outdoor":
+                new_tile = OutdoorTile(tile[0], tile[1])
+                self.available_tiles.append(new_tile)
+            if tile[2] == "Indoor":
+                new_tile = IndoorTile(tile[0], tile[1])
+                self.available_tiles.append(new_tile)
 
 
 class Player:
@@ -70,30 +80,39 @@ class Tile:
     def set_entrance(self, direction):
         self.entrance = direction
 
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
 
 class IndoorTile(Tile):
-    def __init__(self, name, x=0, y=0, effect=None, entrance=None, ex=None):
+    def __init__(self, name, effect=None, x=0, y=0, entrance=None, ex=None):
         self.type = "Indoor"
         super().__init__(name, x, y, effect, entrance, ex)
 
     def __repr__(self):
-        return f'This tile is {self.name}, {self.entrance}, {self.exit}, {self.type}, {self.x} {self.y} {self.effect}'
+        return f'This tile is {self.name}, {self.entrance}, {self.exit}, {self.type},' \
+               f' {self.x} {self.y} {self.effect} \n'
 
 
 class OutdoorTile(Tile):
-    def __init__(self, name, x=0, y=0, effect=None, entrance=None, ex=None):
+    def __init__(self, name, effect=None, x=0, y=0, entrance=None, ex=None):
         self.type = "Outdoor"
         super().__init__(name, x, y, effect, entrance, ex)
 
     def __repr__(self):
-        return f'This tile is {self.name}, {self.entrance}, {self.exit}, {self.type}, {self.x} {self.y} {self.effect}'
+        return f'This tile is {self.name}, {self.entrance}, {self.exit},' \
+               f' {self.type}, {self.x}, {self.y}, {self.effect} \n'
 
 
 def main():
     player = Player()
     game = Game(player)
-    game.load_tile("Foyer", "Indoor")
+    game.load_tiles()
     game.get_game()
+    game.start_game()
 
 
 if __name__ == "__main__":
